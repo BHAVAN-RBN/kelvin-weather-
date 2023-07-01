d148a1b7ff95ead08ecc534521a0fd56a38bab12
BHAVAN-RBN <88610966+BHAVAN-RBN@users.noreply.github.com>
2023-07-01 12:46:56 +0530

    Create app.js
    With our knowledge of JavaScript, let’s convert Kelvin to Celsius, then to Fahrenheit.

---
+++ app.js
@@ -0,0 +1,13 @@
+const Kelvin =293
+//its today weather
+const Celsius = Kelvin-273
+//convert kelvin to celsius
+Fahrenheit = Celsius * (9/5) + 32
+var  fahrenheit =Fahrenheit
+//whole value saved in Fahrenheit
+fahrenheit=Math.floor(fahrenheit)
+//covert value in to faherenhei
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+let Newton = Celsius * (33/100)
+Newton =Math.floor(Newton)
+console.log(`The Newton value ${Newton}.`)
